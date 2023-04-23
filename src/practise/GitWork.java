c7062d20932d64724c453d751ff01104898c761d
Rabin <rabin.shrestha7@gmail.com>
2023-04-23 14:40:49 -0700

    add

--- src/practise/GitWork.java
+++ src/practise/GitWork.java
@@ -5,6 +5,7 @@ public class GitWork {
 	public static void main(String[] args) {
 		System.out.println("helllooooo>>>>");
 		System.out.println("hiiiiiiiiiiiiiii");
+		System.out.println("develop........");
 
 	}
 
